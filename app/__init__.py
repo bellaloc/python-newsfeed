c134d21a9e798b25ab54a2781b0c57e88f1450f7
Christa Lococo <montecitocandleco@gmail.com>
2024-01-14 15:15:52 -0700

    routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
 from flask import Flask
 
 def create_app(test_config=None):
-  # set up app config
-  app = Flask(__name__, static_url_path='/')
-  app.url_map.strict_slashes = False
-  app.config.from_mapping(
-    SECRET_KEY='super_secret_key'
-  )
+    app = Flask(__name__, static_url_path='/')
+    app.url_map.strict_slashes = False
+    app.config.from_mapping(
+        SECRET_KEY='super_secret_key'
+    )
 
-  @app.route('/hello')
-  
-  def hello():
-    return 'hello world'
-  
-  return app
+    @app.route('/hello')
+    def hello():
+        return 'hello world'
+
+    # register routes
+    from .routes.home import bp as home_blueprint
+    app.register_blueprint(home_blueprint)
+
+    return app
