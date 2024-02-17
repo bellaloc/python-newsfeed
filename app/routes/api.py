6b4b2bda2a9a7626eea920b78029a54b0852a4ca
Loc <loc@Locs-Air.lan>
2024-02-17 11:48:42 -0800

    login/logout/

--- app/routes/api.py
+++ app/routes/api.py
@@ -30,3 +30,28 @@ def signup():
         print(e)
         db.rollback()
         return jsonify(message='Signup failed'), 500
+
+@bp.route('/users/logout', methods=['POST'])
+def logout():
+    # remove session variables
+    session.clear()
+    return '', 204
+
+@bp.route('/users/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        user = db.query(User).filter(User.email == data['email']).one()
+        # Check if password matches
+        if user.password == data['password']:
+            session.clear()
+            session['user_id'] = user.id
+            session['loggedIn'] = True
+            return jsonify(id=user.id)
+        else:
+            return jsonify(message='Incorrect credentials'), 400
+    except Exception as e:
+        print(e)
+        return jsonify(message='Incorrect credentials'), 400
