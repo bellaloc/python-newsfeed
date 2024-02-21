1e856253f4b1f3bac2613d5f8ac4e821ae2c9e6b
Loc <loc@Locs-Air.lan>
2024-02-21 12:27:31 -0800

    heroku

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 from flask import Blueprint, render_template
+=======
+from flask import Blueprint, render_template, session, abort
+from app.models import Post
+from app.db import get_db
+from sqlalchemy.orm.exc import NoResultFound
+>>>>>>> parent of d59795f (Merge pull request #16 from bellaloc/API)
 
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
@@ -6,6 +13,10 @@ bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 def dash():
     return render_template('dashboard.html')
 
+<<<<<<< HEAD
 @bp.route('/edit/<id>')
+=======
+@bp.route('/edit/<int:id>')  
+>>>>>>> parent of d59795f (Merge pull request #16 from bellaloc/API)
 def edit(id):
     return render_template('edit-post.html')
