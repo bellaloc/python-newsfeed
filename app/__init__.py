aa99318480e3aeb516b4f3c97e53a1d93ac7922b
Loc <loc@Locs-Air.lan>
2024-02-21 09:42:44 -0800

    init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,15 @@
+# Importing necessary modules
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from app.routes import home_bp, dashboard_bp, api_bp  # Importing blueprints from routes package
 from app.db import init_db  # Import the init_db function
 from app.utils import filters
-import os
-from dotenv import load_dotenv
 
 # Load environment variables from .env file
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
-
 
 def create_app(test_config=None):
     app = Flask(__name__, static_url_path='/')
@@ -18,8 +18,11 @@ def create_app(test_config=None):
     app.jinja_env.filters['format_date'] = filters.format_date
     app.jinja_env.filters['format_plural'] = filters.format_plural
     app.config.from_mapping(
-        SECRET_KEY=os.getenv('SECRET_KEY', 'super_secret_key')
+        SECRET_KEY='super_secret_key'
     )
+
+    # Retrieve the value of the DB_URL environment variable
+    db_url = os.getenv("DB_URL")
 
     @app.route('/hello')
     def hello():
