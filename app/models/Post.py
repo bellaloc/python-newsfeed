6b1b31a2aa901924eda92b3080f24c8c343fe689
Loc <loc@Locs-Air.lan>
2024-02-21 10:58:54 -0800

    models

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from app.db import Base
 from .Vote import Vote
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.hybrid import hybrid_property
 
@@ -30,3 +30,17 @@ class Post(Base):
             .where(Vote.post_id == cls.id)
             .label('vote_count')
         )
+
+class Article(Base):
+    __tablename__ = 'articles'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    content = Column(String, nullable=False)
+    author_id = Column(Integer, ForeignKey('users.id'))
+    
+    # Define the relationship with the User model
+    author = relationship("User", back_populates="articles")
+    
+    # Define the relationship with the Comment model
+    comments = relationship("Comment", back_populates="article")
